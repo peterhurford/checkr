b80e203429851f046dff8a3699b5dc6bcbd4d885
Peter Hurford <peter@peterhurford.com>
2016-04-05 10:37:15 -0500

    test for pathing this way

--- tests/testthat/test-package-exports-checked.R
+++ tests/testthat/test-package-exports-checked.R
@@ -3,6 +3,7 @@ context("pacakge_exports_checked")
 test_that("it passes when all exported functions are checked", {
   devtools::install("fakepackages/allexportedchecked")
   expect_true(package_exports_checked("allexportedchecked"))
+  expect_true(package_exports_checked("fakepackages/allexportedchecked"))
   remove.packages("allexportedchecked")
 })
 
@@ -11,14 +12,11 @@ test_that("it fails when all exported functions are not checked", {
   expect_false(package_exports_checked("notallexportedchecked", stop = FALSE))
   expect_error(package_exports_checked("notallexportedchecked"),
     "not checked by checkr: pending_identity")
+  expect_false(package_exports_checked("fakepackages/notallexportedchecked", stop = FALSE))
   remove.packages("notallexportedchecked")
 })
 
 test_that("for example, it doesn't work on checkr", {
   expect_false(package_exports_checked("checkr", stop = FALSE))
   expect_error(package_exports_checked("checkr"), "not checked by checkr")
-})
-
-test_that("it takes package paths", {
-  expect_false(package_exports_checked("..", stop = FALSE))
 })
